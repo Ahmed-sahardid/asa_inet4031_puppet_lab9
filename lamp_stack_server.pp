56f0bfed25b552c510dd294a8db3e1d772d9ee9b
Ahmed Sahardid <sahar021@umn.edu>
2025-11-11 02:10:05 +0000

    INET4031: Puppet LAMP stack + users manifests

---
+++ lamp_stack_server.pp
@@ -0,0 +1,52 @@
+# === Apache and PHP Packages ===
+package { 'apache2':
+  ensure => installed,
+}
+
+package { 'php':
+  ensure  => installed,
+  notify  => Service['apache2'],
+  require => Package['apache2'],
+}
+
+package { 'libapache2-mod-php':
+  ensure  => installed,
+  require => [Package['apache2'], Package['php']],
+}
+
+package { 'php-cli':
+  ensure  => installed,
+  require => [Package['apache2'], Package['php']],
+}
+
+package { 'php-mysql':
+  ensure  => installed,
+  require => [Package['apache2'], Package['php']],
+}
+
+# === PHP Test Page ===
+file { '/var/www/html/phpinfo.php':
+  source  => '/home/asah2201/asa_inet4031_puppet_lab9/phpinfo.php',
+  notify  => Service['apache2'],
+  require => Package['apache2'],
+}
+
+
+# === Apache Service ===
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => [Package['apache2'], Package['php']],
+}
+
+# === MariaDB (MySQL) Setup ===
+package { 'mariadb-server':
+  ensure => installed,
+}
+
+service { 'mariadb':
+  ensure  => running,
+  enable  => true,
+  require => Package['mariadb-server'],
+}
+
